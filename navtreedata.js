86a4a4041e28617425791b8db0bb70a08773006e
Akos Pasztor <akospasztor@users.noreply.github.com>
2019-12-23 13:18:48 +0000

    Deploy to GitHub Pages

---
+++ navtreedata.js
@@ -0,0 +1,77 @@
+var NAVTREE =
+[
+  [ "STM32 Bootloader", "index.html", [
+    [ "Changelog for STM32 Bootloader", "autotoc_md7.html", [
+      [ "Table of Contents", "index.html#autotoc_md1", null ],
+      [ "Bootloader features", "index.html#autotoc_md2", null ],
+      [ "Examples", "index.html#autotoc_md3", null ],
+      [ "How to use", "index.html#autotoc_md4", null ],
+      [ "Configuration", "index.html#autotoc_md5", null ],
+      [ "References", "index.html#autotoc_md6", [
+        [ "[Unreleased]", "autotoc_md7.html#autotoc_md8", null ],
+        [ "1.1.0", "autotoc_md7.html#autotoc_md9", [
+          [ "Added", "autotoc_md7.html#autotoc_md10", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md11", null ],
+          [ "Fixed", "autotoc_md7.html#autotoc_md12", null ]
+        ] ],
+        [ "1.08", "autotoc_md7.html#autotoc_md13", [
+          [ "Added", "autotoc_md7.html#autotoc_md14", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md15", null ],
+          [ "Fixed", "autotoc_md7.html#autotoc_md16", null ]
+        ] ],
+        [ "1.07", "autotoc_md7.html#autotoc_md17", [
+          [ "Added", "autotoc_md7.html#autotoc_md18", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md19", null ]
+        ] ],
+        [ "1.06", "autotoc_md7.html#autotoc_md20", [
+          [ "Added", "autotoc_md7.html#autotoc_md21", null ],
+          [ "Fixed", "autotoc_md7.html#autotoc_md22", null ]
+        ] ],
+        [ "1.05", "autotoc_md7.html#autotoc_md23", [
+          [ "Added", "autotoc_md7.html#autotoc_md24", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md25", null ]
+        ] ],
+        [ "1.04", "autotoc_md7.html#autotoc_md26", [
+          [ "Added", "autotoc_md7.html#autotoc_md27", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md28", null ],
+          [ "Fixed", "autotoc_md7.html#autotoc_md29", null ]
+        ] ],
+        [ "1.03", "autotoc_md7.html#autotoc_md30", [
+          [ "Added", "autotoc_md7.html#autotoc_md31", null ],
+          [ "Changed", "autotoc_md7.html#autotoc_md32", null ]
+        ] ],
+        [ "1.02", "autotoc_md7.html#autotoc_md33", [
+          [ "Fixed", "autotoc_md7.html#autotoc_md34", null ]
+        ] ],
+        [ "1.01", "autotoc_md7.html#autotoc_md35", [
+          [ "Added", "autotoc_md7.html#autotoc_md36", null ],
+          [ "Fixed", "autotoc_md7.html#autotoc_md37", null ]
+        ] ],
+        [ "1.00", "autotoc_md7.html#autotoc_md38", [
+          [ "Added", "autotoc_md7.html#autotoc_md39", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"autotoc_md7.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['changelog_20for_20stm32_20bootloader',['Changelog for STM32 Bootloader',['../autotoc_md7.html',1,'']]]
+];
